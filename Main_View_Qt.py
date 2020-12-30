be030078bf938ab6cc427ae4ccdccb5c033a503b
Bill2015 <jimmy3112015@gmail.com>
2020-12-31 07:17:47 +0800

    Make detection more smooth
    Change:
        - Add a HistoryDict class to make detection more smooth
        - Change the way of vertical and horizon head angle calculation

--- Main_View_Qt.py
+++ Main_View_Qt.py
@@ -23,6 +23,7 @@ qtCreatorFile = path + OS.sep + "ui" + OS.sep + "mainView.ui"
 # 讀入用Qt Designer設計的GUI layout
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)   
 
+
 # Python的多重繼承 MainUi 繼承自兩個類別
 class MainUi(QtWidgets.QMainWindow, Ui_MainWindow):
    
@@ -44,16 +45,20 @@ class MainUi(QtWidgets.QMainWindow, Ui_MainWindow):
         self.label_verticleThreshold= self.findChild(QLabel, name='labelText_Angle_Threshold_Verticle') # 顯示垂直閥值的 Label
         self.scrollBar_horizon      = self.findChild(QSlider, name='sliderBar_Threshold_Horizon')    # 水平閥值
         self.scrollBar_verticle     = self.findChild(QSlider, name='sliderBar_Threshold_Verticle')   # 垂直閥值
-        self.label_distractRate     = self.findChild(QLabel, name='labelShow_Distract_Rate')        # 分心度
+        self.label_focusRate        = self.findChild(QLabel, name='labelShow_Focus_Rate')        # 專注度
 
+        # 初始化影片顯示區域
         self.initVideoEvent()
         
         
         self.update_flag = True     # 是否要繼續更新旗標
         self.fps_rate = 0           # FPS
         self.pre_time = TIME.time() # 取得目前時間
-        self.pre_direction      = 0 # 上一個頭的動作
-        self.oscillate_count    = 0 # 震盪計算
+
+        self.hRotation          = 0 # 水平轉向值
+        self.vRotation          = 0 # 垂直轉向值
+        self.focus_count        = 0
+        self.focus_timer        = 0
 
         # 多執行續，用來更新圖片
         thread = THREAD.Thread(target = self.update_Image, args=( ))
@@ -64,21 +69,43 @@ class MainUi(QtWidgets.QMainWindow, Ui_MainWindow):
         pixmap = QPixmap('image.jpg')
         self.label_imageView.setPixmap(pixmap)
 
-    # 計算 FPS 的函式
-    def _caculate_fps(self):
-        # 計算 FPS
-        self.fps_rate += 1
-        now_time = TIME.time()
-        if (now_time - self.pre_time) > 1:
-            self.label_fpsVal.setText( str( self.fps_rate ) )
-            self.fps_rate = 0
-            self.pre_time = now_time
-        
 
+    def updateUI_second(self):
+
+        avg_hRotation = round( self.hRotation / self.fps_rate, 2)
+        avg_vRotation = round( self.vRotation / self.fps_rate, 2)
+        # 更新頭部旋轉數值
+        self.label_horizonVal.setText(  str( avg_hRotation ) )
+        self.label_verticleVal.setText( str( avg_vRotation ) )
+
+        # 取得目前 ScrollBar 的值
+        hThreshold = self.scrollBar_horizon.value()
+        vThreshold = self.scrollBar_verticle.value()
+        
+        # 取得目前頭部的轉向
+        (head_rotation, head_rotation_str) = HEAD.judge_look(horizon_angle       =avg_hRotation, vertical_angle    =avg_vRotation, 
+                                                             horizon_threshold   =hThreshold   , vertical_threshold=vThreshold)
+        
+        # ------------------------------------------------------------
+        rotation_str = '目前頭部方向： ' + str(head_rotation_str)
+        self.label_headDirection.setText( rotation_str )
+
+        self.hRotation = 0
+        self.vRotation = 0
+
+        # ------------------------------------------------------------
+        if head_rotation == HEAD.LEFT or head_rotation == HEAD.RIGHT or head_rotation == HEAD.GONE:
+            self.focus_count += 1
+        self.focus_timer += 1
+
+        self.label_focusRate.setText( str( round( (1 - (self.focus_count / self.focus_timer)) * 100, 2)  ) + '%' )
+    
     # 更新影像函式
     def update_Image(self):
         # 開始偵測
         while self.update_flag:
+            QtWidgets.QApplication.instance().processEvents()
+            # ------------------------------------------------------------
             # 偵測頭部旋轉，並取得數值
             (horizon_angle, vertical_angle, cvimg) = HEAD.detect()
 
@@ -90,9 +117,8 @@ class MainUi(QtWidgets.QMainWindow, Ui_MainWindow):
             except:
                 pass
 
-            # 更新頭部旋轉數值
-            self.label_horizonVal.setText( str(horizon_angle) )
-            self.label_verticleVal.setText( str(vertical_angle) )
+            self.hRotation += horizon_angle
+            self.vRotation += vertical_angle
 
             # ------------------------------------------------------------
             # 取得目前 ScrollBar 的值
@@ -101,25 +127,20 @@ class MainUi(QtWidgets.QMainWindow, Ui_MainWindow):
             self.label_horizonThreshold.setText('水平閥值：     ' + str(hThreshold) )
             self.label_verticleThreshold.setText('垂直閥值：     ' + str(vThreshold) )
 
-            # 取得目前頭部的轉向
-            (head_rotation, head_rotation_str) = HEAD.judge_look(horizon_angle       =horizon_angle  , vertical_angle    =vertical_angle, 
-                                                                 horizon_threshold   =hThreshold     , vertical_threshold=vThreshold)
+ 
+
+            # ------------------------------------------------------------    
+            # 計算 FPS
+            self.fps_rate += 1
+            now_time = TIME.time()
+            if (now_time - self.pre_time) > 1:
+                self.updateUI_second()
+
+                self.label_fpsVal.setText( str( self.fps_rate ) )
+                self.fps_rate = 0
+                self.pre_time = now_time
 
             
-            # ------------------------------------------------------------
-            if head_rotation != self.pre_direction :
-                self.oscillate_count += 1
-                if( self.oscillate_count >= 2 ):
-                    self.pre_direction = head_rotation
-                    rotation_str = '目前頭部方向： ' + str(head_rotation_str)
-                    self.label_headDirection.setText( rotation_str )
-            else:
-                self.oscillate_count = 0
-            
-            
-            # ------------------------------------------------------------
-            # 計算 FPS
-            self._caculate_fps()
 
 
 
--- head_rotation_detect.py
+++ head_rotation_detect.py
@@ -3,6 +3,7 @@ import numpy as np
 import dlib as dlib
 import math
 
+from HistoryDict import *
 # 將特徵點的座標轉成陣列
 def shape_to_np(shape, dtype = "int"):
 	# initialize the list of (x, y)-coordinates
@@ -43,7 +44,53 @@ def get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val):
                                       dist_coeffs)
     point_2d = np.int32(point_2d.reshape(-1, 2))
     return point_2d
+def draw_annotation_box(img, rotation_vector, translation_vector, camera_matrix,
+                        rear_size=100, rear_depth=0, front_size=500, front_depth=100,
+                        color=(255, 255, 0), line_width=2):
+    """
+    Draw a 3D anotation box on the face for head pose estimation
+    Parameters
+    ----------
+    img : np.unit8
+        Original Image.
+    rotation_vector : Array of float64
+        Rotation Vector obtained from cv2.solvePnP
+    translation_vector : Array of float64
+        Translation Vector obtained from cv2.solvePnP
+    camera_matrix : Array of float64
+        The camera matrix
+    rear_size : int, optional
+        Size of rear box. The default is 300.
+    rear_depth : int, optional
+        The default is 0.
+    front_size : int, optional
+        Size of front box. The default is 500.
+    front_depth : int, optional
+        Front depth. The default is 400.
+    color : tuple, optional
+        The color with which to draw annotation box. The default is (255, 255, 0).
+    line_width : int, optional
+        line width of lines drawn. The default is 2.
+    Returns
+    -------
+    None.
+    """
     
+    rear_size = 1
+    rear_depth = 0
+    front_size = img.shape[1]
+    front_depth = front_size*2
+    val = [rear_size, rear_depth, front_size, front_depth]
+    point_2d = get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val)
+    # # Draw all the lines
+    cv2.polylines(img, [point_2d], True, color, line_width, cv2.LINE_AA)
+    cv2.line(img, tuple(point_2d[1]), tuple(
+        point_2d[6]), color, line_width, cv2.LINE_AA)
+    cv2.line(img, tuple(point_2d[2]), tuple(
+        point_2d[7]), color, line_width, cv2.LINE_AA)
+    cv2.line(img, tuple(point_2d[3]), tuple(
+        point_2d[8]), color, line_width, cv2.LINE_AA)
+
 def head_pose_points(img, rotation_vector, translation_vector, camera_matrix):
     """
     Get the points to estimate head pose sideways    
@@ -107,7 +154,7 @@ CAMERA_MATRIX = np.array(
                                 [focal_length, 0, center[0]],
                                 [0, focal_length, center[1]],
                                 [0, 0, 1]
-                            ], dtype='float'
+                            ], dtype='double'
                         )
 
 def rect_to_bb(rect):
@@ -122,7 +169,63 @@ def rect_to_bb(rect):
     # return a tuple of (x, y, w, h)
     return (x, y, w, h)
 
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( roll and yaw are swapped ).
+def get_angles_eular(rvec, tvec):
+    rmat = cv2.Rodrigues(rvec)[0]
+    P = np.hstack((rmat, tvec)) # projection matrix [R | t]
+    degrees = -cv2.decomposeProjectionMatrix(P)[6]
+    rx, ry, rz = degrees[:, 0]
+    return [rx, ry, rz]
+
+def get_angles_gerneal(image_points, rvec, tvec):
+    dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+    # 將 3D 座標點，投影至 2D 圖片面板上
+    (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rvec, tvec, CAMERA_MATRIX, dist_coeffs, cv2.SOLVEPNP_AP3P)
+
+    # --------------------------------------------------------------------------------------
+    # 取得垂直角度
+    # 取得鼻尖座標 (臉部直接投影位置)
+    p1 = ( int(image_points[0][0]), int(image_points[0][1]))
+    # 取得鼻尖座標 (臉部 3D 投影後的座標)
+    p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+
+    cv2.line(img, p1, p2, (0, 255, 0), 2)
+    try:
+        m = (p2[1] - p1[1])/(p2[0] - p1[0])
+        vertical_angle = int(math.degrees(math.atan(m)))
+    except:
+        vertical_angle = 0
+    
+    # cv2.putText(img, str(vertical_angle), tuple(p1), font, 2, (128, 255, 255), 3)
+    # --------------------------------------------------------------------------------------
+    # 取得水平角度
+    x1, x2 = head_pose_points(img, rvec, tvec, CAMERA_MATRIX)
+
+    cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
+    # for (x, y) in marks:
+    #     cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
+    # cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
+    try:
+        if (x2[0] - x1[0]) != 0:
+            m = (x2[1] - x1[1])/(x2[0] - x1[0])
+            horizon_angle = int(math.degrees(math.atan(-1/m)))
+        else: 
+            horizon_angle = 0
+    except:
+        horizon_angle = 0
+    # cv2.putText(img, str(horizon_angle), tuple(x1), font, 2, (255, 255, 128), 3)
+    return (horizon_angle, vertical_angle)
+
+
+# ==================== 偵測函式 ====================
+
+Pnp_History = History( 2 )
+Dib_History = History( 6 )
+
 def detect():
+
     vertical_angle = 0
     horizon_angle  = 0
     # 讀取攝像頭圖片
@@ -137,87 +240,56 @@ def detect():
         for face in faces:       # 取得多張人臉
 
             (x, y, w, h) = rect_to_bb(face)
-            cv2.rectangle(img,(x, y),(x + w, y + h),(255,0,0),2)
+            # cv2.rectangle(img,(x, y),(x + w, y + h),(255,0,0),2)
 
             marks = FACE_PREDICTOR(img, face)    # 使用 dlib 模型，將臉部位置標記出
             marks = shape_to_np(marks)
 
             # mark_detector.draw_marks(img, marks, color=(0, 255, 0))
 
+            # 儲存 dlib 標記結果取平均，讓整體更滑順
+            landmark = [marks[30], marks[8], marks[36], marks[45], marks[48], marks[54]]
+            Dib_History.add( landmark )
+            if( Dib_History.is_full() ):
+                landmark = Dib_History.get_average()
+                Dib_History.pop()
+
+
             # 建立特徵點陣列 (點由 Dlib 產生)
             image_points = np.array([
-                                    marks[30],     # 取得鼻尖點
-                                    marks[8],      # 取得下巴點
-                                    marks[36],     # 取得左眼左眼角
-                                    marks[45],     # 取得右眼右眼角
-                                    marks[48],     # 取得左嘴角
-                                    marks[54]      # 取得右嘴角
+                                    landmark[0],     # 取得鼻尖點
+                                    landmark[1],      # 取得下巴點
+                                    landmark[2],     # 取得左眼左眼角
+                                    landmark[3],     # 取得右眼右眼角
+                                    landmark[4],     # 取得左嘴角
+                                    landmark[5]      # 取得右嘴角
                                 ], 
                                 dtype='double')
-            dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
 
+
+
+            dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
             # 取得在 3D 投影至 2D 畫面時的座標運算，用來計算 3D 位置標定的移動情況
             (success, rotation_vector, translation_vector) = cv2.solvePnP(FACE_3D_MODEL_POINT, image_points, CAMERA_MATRIX, dist_coeffs)
             
 
+            # 儲存 pnp 運算結果取平均，讓整體更滑順
+            Pnp_History.add( [rotation_vector, translation_vector] )
+            if( Pnp_History.is_full() ):
+                [rotation_vector, translation_vector] = Pnp_History.get_average()
+                Pnp_History.pop()
+
+
+            draw_annotation_box(img, rotation_vector, translation_vector, CAMERA_MATRIX)
             # Project a 3D point (0, 0, 1000.0) onto the image plane.
             # We use this to draw a line sticking out of the nose
             
-            # 將 3D 座標點，投影至 2D 圖片面板上
-            (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, CAMERA_MATRIX, dist_coeffs)
-            
+
             # 將臉部的點標記出至圖片上
             for p in image_points:
                 cv2.circle(img, (int(p[0]), int(p[1])), 3, (0,0,255), -1)
             
-            
-            # --------------------------------------------------------------------------------------
-            # 取得垂直角度
-            # 取得鼻尖座標 (臉部直接投影位置)
-            p1 = ( int(image_points[0][0]), int(image_points[0][1]))
-            # 取得鼻尖座標 (臉部 3D 投影後的座標)
-            p2 = ( int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
-
-            cv2.line(img, p1, p2, (0, 255, 0), 2)
-            try:
-                m = (p2[1] - p1[1])/(p2[0] - p1[0])
-                vertical_angle = int(math.degrees(math.atan(m)))
-            except:
-                vertical_angle = 0
-
-            # if vertical_angle >= 48:
-            #     print('Head down')
-            #     cv2.putText(img, 'Head down', (30, 30), font, 2, (255, 255, 128), 3)
-            # elif vertical_angle <= -48:
-            #     print('Head up')
-            #     cv2.putText(img, 'Head up', (30, 30), font, 2, (255, 255, 128), 3)
-
-            # cv2.putText(img, str(vertical_angle), tuple(p1), font, 2, (128, 255, 255), 3)
-            # --------------------------------------------------------------------------------------
-            # 取得水平角度
-            x1, x2 = head_pose_points(img, rotation_vector, translation_vector, CAMERA_MATRIX)
-
-            cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
-            # for (x, y) in marks:
-            #     cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
-            # cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
-                
-            try:
-                m = (x2[1] - x1[1])/(x2[0] - x1[0])
-                horizon_angle = int(math.degrees(math.atan(-1/m)))
-            except:
-                horizon_angle = 90
-                
-                # print('div by zero error')
-             
-            # if horizon_angle >= 48:
-            #     print('Head right')
-            #     cv2.putText(img, 'Head right', (90, 30), font, 2, (255, 255, 128), 3)
-            # elif horizon_angle <= -48:
-            #     print('Head left')
-            #     cv2.putText(img, 'Head left', (90, 30), font, 2, (255, 255, 128), 3)
-            
-            # cv2.putText(img, str(horizon_angle), tuple(x1), font, 2, (255, 255, 128), 3)
+            (horizon_angle, vertical_angle) = get_angles_gerneal(image_points, rotation_vector, translation_vector) 
 
         # 顯示影像
         # cv2.imshow('img', img)
@@ -231,6 +303,7 @@ LEFT    = 1
 RIGHT   = 2
 UP      = 3
 DOWN    = 4
+GONE    = 5
 
 # 判斷頭部目前的轉向
 def judge_look(horizon_angle, vertical_angle, horizon_threshold, vertical_threshold):
@@ -238,27 +311,28 @@ def judge_look(horizon_angle, vertical_angle, horizon_threshold, vertical_thresh
     head_direction_str  = ['      ','      '] 
 
     # print('div by zero error')
-    if abs(vertical_angle) <= 90 - vertical_threshold :
-        print('Head down')
+    if abs(vertical_angle) <= 45 - (vertical_threshold / 2) :
+        # print('Head down')
         head_direction = UP
         head_direction_str[1] = ' 向下 '
 
-    elif abs(vertical_angle) >= vertical_threshold:
-        print('Head up')
+    elif abs(vertical_angle) >= 45 + (vertical_threshold / 2):
+        # print('Head up')
         head_direction = DOWN
         head_direction_str[1] = ' 向上 '
 
     if horizon_angle >= horizon_threshold:
-        print('Head right')
+        # print('Head right')
         head_direction = RIGHT
         head_direction_str[0] = ' 向右 '
 
     elif horizon_angle <= -horizon_threshold:
-        print('Head left')
+        # print('Head left')
         head_direction = LEFT
         head_direction_str[0] = ' 向左 '
 
-
+    if horizon_angle == 0 and vertical_angle == 0:
+        head_direction = GONE
 
     return (head_direction, head_direction_str)
 
